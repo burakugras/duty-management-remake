54a9400d616c682e6e44e75e4de57cc1b5d7d6e5
burakugras <burakugras67@gmail.com>
2024-12-29 22:25:34 +0300

    added rest of todoItem

--- components/todoItem.js
+++ components/todoItem.js
@@ -6,9 +6,6 @@ export function renderTodoDiv(todo) {
     const leftDiv = document.createElement('div');
     leftDiv.classList.add('left-div');
 
-    const rightDiv = document.createElement('div');
-    rightDiv.classList.add('right-div');
-
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
     checkbox.checked = todo.status === 'Completed';
@@ -24,6 +21,45 @@ export function renderTodoDiv(todo) {
     leftDiv.appendChild(checkbox);
     leftDiv.appendChild(span);
     leftDiv.appendChild(editInput);
+
+    const middleDiv = document.createElement('div');
+    middleDiv.classList.add('middle-div');
+
+    const progressStatus = document.createElement('div');
+    progressStatus.classList.add('progress-status');
+
+    const createDateSpan = document.createElement('span');
+    createDateSpan.classList.add('create-date');
+    createDateSpan.textContent = todo.createDate; //?
+
+    middleDiv.appendChild(progressStatus);
+    middleDiv.appendChild(createDateSpan);
+
+    const rightDiv = document.createElement('div');
+    rightDiv.classList.add('right-div');
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('todo-deleteBtn');
+    deleteBtn.textContent = 'Delete';
+
+    const editBtn = document.createElement('button');
+    deleteBtn.classList.add('todo-editBtn');
+    editBtn.textContent = 'Edit';
+
+    const saveBtn = document.createElement('button');
+    saveBtn.classList.add('todo-saveBtn');
+    saveBtn.textContent = 'Save';
+    saveBtn.hidden = true;
+
+    rightDiv.appendChild(deleteBtn);
+    rightDiv.appendChild(editBtn);
+    rightDiv.appendChild(saveBtn);
+
+    todoDiv.appendChild(leftDiv);
+    todoDiv.appendChild(middleDiv);
+    todoDiv.appendChild(rightDiv);
+
+    //Event Listeners
 
 
 }
